d1e05f2d7557afba00d7dee2e8e475e5e894ec04
Arosy <arosy@live.de>
2025-02-24 16:07:34 +0100

    initial commit

---
+++ run-ansible.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+if [ -z "$1" ]; then
+  echo "PLEASE SPECIFY INVENTORY! Either 'devel' OR 'prod'"
+  exit 1
+fi
+
+pushd playbooks
+ansible-playbook --inventory-file ../inv/$1 install-docker.yml \
+											install-portainer.yml \
+											create-stack-pihole.yml \
+											create-stack-traefik.yml \
+											create-stack-dashboard.yml \
+											create-stack-jdownloader2.yml \
+											create-stack-mqtt.yml \
+											create-stack-nodered.yml \
+											create-stack-vaultwarden.yml \
+											install-nmap.yml \
+											install-zerotier.yml \
+											config-firewall.yml \
+                                            --ask-pass --ask-become-pass
